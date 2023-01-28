cb0db143c613f0765b8392dac8b336e642e79738
racarlos <rarcarlos09@gmail.com>
2023-01-28 14:53:34 +0800

    Fix packages and change redis pass

--- shopeerium-server/repositories/shopRepository.js
+++ shopeerium-server/repositories/shopRepository.js
@@ -1,55 +1,48 @@
-const RepositoryError = require('../HttpCodes/RepositoryError')
+const RepositoryError = require("../HttpCodes/RepositoryError");
 
-const Redis = require('ioredis');
-const redis =  new Redis({
-    port: 6379,
-    host: 'shopeerium_redis',
-    password: "Password097"
-}); 
+const Redis = require("ioredis");
+const redis = new Redis({
+  port: 6379,
+  host: "shopeerium_redis",
+  password: "securepassword",
+});
 
+const shopRepository = function () {
+  const repository = {
+    selectShops: async () => {
+      // Get all Shops
+      try {
+        return await redis.keys(`shops:*`);
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`REDIS - Select all Shops Failed`);
+      }
+    },
 
-const shopRepository = function(){
+    selectShop: async (sellerUsername) => {
+      // Get a single shop
+      try {
+        return await redis.hgetall(`shops:${sellerUsername}`);
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Select Shops: ${sellerUsername} Failed`);
+      }
+    },
 
-    const repository = {
+    insertShop: async (newShop) => {
+      // Insert new shop
+      try {
+        return await redis.hset(`shops:${newShop.sellerUsername}`, newShop);
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(
+          `Insert Shop of ${newShop.sellerUsername} Failed`
+        );
+      }
+    },
+  };
 
-        selectShops: async() => {                           // Get all Shops
-            try {
-                
-                return await redis.keys(`shops:*`);
-
-            } catch (error) {
-                console.error(error);
-                throw new RepositoryError(`REDIS - Select all Shops Failed`);
-            }
-        },
-
-        selectShop: async(sellerUsername) => {                  // Get a single shop
-            try {
-                
-                return await redis.hgetall(`shops:${sellerUsername}`);
-
-
-            } catch (error) {
-                console.error(error);
-                throw new RepositoryError(`Select Shops: ${sellerUsername} Failed`);
-            }
-        },
-
-        insertShop: async(newShop) => {               // Insert new shop 
-            try {
-
-                return await redis.hset(`shops:${newShop.sellerUsername}`,newShop);
-
-            } catch (error) {
-                console.error(error);
-                throw new RepositoryError(`Insert Shop of ${newShop.sellerUsername} Failed`);
-            }
-        }
-
-    };
-    
-
-    return repository;
-}
+  return repository;
+};
 
 module.exports = shopRepository;
--- shopeerium-server/repositories/userRepository.js
+++ shopeerium-server/repositories/userRepository.js
@@ -1,119 +1,127 @@
-const knex = require('./knex');
-const RepositoryError = require('../HttpCodes/RepositoryError')
+const knex = require("./knex");
+const RepositoryError = require("../HttpCodes/RepositoryError");
 
-const Redis = require('ioredis');
-const redis =  new Redis({
-    port: 6379,
-    host: 'shopeerium_redis',
-    password: "Password097"
-}); 
+const Redis = require("ioredis");
+const redis = new Redis({
+  port: 6379,
+  host: "shopeerium_redis",
+  password: "securepassword",
+});
 
-const userRepository = function(){
-    const repository = {                // Object of List of DB Functions 
+const userRepository = function () {
+  const repository = {
+    // Object of List of DB Functions
 
-        verifyUser: async(username,password) => {
-            try{
+    verifyUser: async (username, password) => {
+      try {
+        const data = await knex.raw(`CALL verifyUser(?,?)`, [
+          username,
+          password,
+        ]);
+        return data[0][0][0]; // ok packet -> data packet -> nested data
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Verify User: ${username} for login Failed`);
+      }
+    },
 
-                const data = await knex.raw(`CALL verifyUser(?,?)`,[username,password])
-                return data[0][0][0]        // ok packet -> data packet -> nested data 
-                
-            } catch (error) {
-                console.error(error)
-                throw new RepositoryError(`Verify User: ${username} for login Failed`)
-            }
-        }, 
+    selectUsers: async () => {
+      try {
+        const data = await knex.raw(`CALL selectUsers();`);
+        return data[0][0];
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError("SELECT all Users Failed");
+      }
+    },
 
-        selectUsers: async () => {
-            try{
-                const data = await knex.raw(`CALL selectUsers();`);
-                return data [0][0]
-            } catch(error) {
-                console.error(error)
-                throw new RepositoryError('SELECT all Users Failed')
-            }
-        },
+    selectUser: async (username) => {
+      try {
+        return await redis.hgetall(`users:${username}`);
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`SELECT User: ${username} Failed`);
+      }
+    },
 
-        selectUser: async(username) => {
-            try{
-                return await redis.hgetall(`users:${username}`);
-         
-            } catch(error){
-                console.error(error)
-                throw new RepositoryError(`SELECT User: ${username} Failed`)
-            }
-        },
-        
-        insertUser: async(newUser)=> {
-            try {
-                
-                const sqlTask = knex.raw(`CALL insertUser(?,?,?,?,?,?,?,?)`,[newUser.username,newUser.firstName,
-                newUser.lastName,newUser.number,newUser.role,newUser.email,newUser.password,newUser.imgUrl]);
-                
-                const redisTask = redis.hset(`users:${newUser.username}`,newUser);
-                
-                const results = await Promise.all([sqlTask,redisTask]);
+    insertUser: async (newUser) => {
+      try {
+        const sqlTask = knex.raw(`CALL insertUser(?,?,?,?,?,?,?,?)`, [
+          newUser.username,
+          newUser.firstName,
+          newUser.lastName,
+          newUser.number,
+          newUser.role,
+          newUser.email,
+          newUser.password,
+          newUser.imgUrl,
+        ]);
 
-                return results[0];
-                
+        const redisTask = redis.hset(`users:${newUser.username}`, newUser);
 
-            } catch (error) {
-                console.error(error)
-                throw new RepositoryError(`Insert User: ${username} Failed`)
-            }
-        },
+        const results = await Promise.all([sqlTask, redisTask]);
 
-        deleteBuyer: async(username)=> {
-            try{
-                const sqlTask =  knex.raw(`CALL deleteBuyer(?)`,[username]);
-                const redisTask =  redis.del(`users:${username}`);
+        return results[0];
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Insert User: ${username} Failed`);
+      }
+    },
 
-                const results = await Promise.all([sqlTask,redisTask]);
+    deleteBuyer: async (username) => {
+      try {
+        const sqlTask = knex.raw(`CALL deleteBuyer(?)`, [username]);
+        const redisTask = redis.del(`users:${username}`);
 
-                return results[0];
-                
+        const results = await Promise.all([sqlTask, redisTask]);
 
-            } catch (error){    
-                console.error(error)
-                throw new RepositoryError(`Delete Buyer: ${username} Failed`)
-            }
-        },
+        return results[0];
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Delete Buyer: ${username} Failed`);
+      }
+    },
 
-        deleteSeller: async(username)=> {
-            try{
-                const sqlTask = knex.raw(`CALL deleteSeller(?)`,[username]);
-                const redisTask = redis.del(`users:${username}`);              
-            
+    deleteSeller: async (username) => {
+      try {
+        const sqlTask = knex.raw(`CALL deleteSeller(?)`, [username]);
+        const redisTask = redis.del(`users:${username}`);
 
+        const results = await Promise.all([sqlTask, redisTask]);
 
-                const results = await Promise.all([sqlTask,redisTask]);
+        return results[0];
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Delete Seller: ${username} Failed`);
+      }
+    },
 
-                return results[0];
- 
-            } catch (error){    
-                console.error(error)
-                throw new RepositoryError(`Delete Seller: ${username} Failed`)
-            }
-        },
+    updateUser: async (updateUser) => {
+      try {
+        const sqlTask = knex.raw(`CALL updateUser(?,?,?,?,?,?,?)`, [
+          updateUser.username,
+          updateUser.firstName,
+          updateUser.lastName,
+          updateUser.number,
+          updateUser.email,
+          updateUser.password,
+          updateUser.imgUrl,
+        ]);
+        const redisTask = redis.hset(
+          `users:${updateUser.username}`,
+          updateUser
+        );
 
-        updateUser: async(updateUser) => {
+        const results = await Promise.all(sqlTask, redisTask);
+        return results[0];
+      } catch (error) {
+        console.error(error);
+        throw new RepositoryError(`Update User: ${username} Failed`);
+      }
+    },
+  };
 
-            try{
-                const sqlTask =  knex.raw(`CALL updateUser(?,?,?,?,?,?,?)`,[updateUser.username,updateUser.firstName,
-                updateUser.lastName,updateUser.number,updateUser.email,updateUser.password,updateUser.imgUrl]);
-                const redisTask = redis.hset(`users:${updateUser.username}`,updateUser); 
-                
-                const results = await Promise.all(sqlTask,redisTask);
-                return results[0];
-
-            } catch (error){
-                console.error(error)
-                throw new RepositoryError(`Update User: ${username} Failed`)
-            }
-        }
-    }
-
-
-    return repository;
-}
+  return repository;
+};
 
 module.exports = userRepository;
